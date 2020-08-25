28cfaf7b4435704ed5760726bac35d3391a2e37d
Guilherme Reis <guilhermejosereis@outlook.com>
2020-08-24 20:14:54 -0400

    Initial Commit

--- App.js
+++ App.js
@@ -1,21 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import axios from "axios";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [location, setLocation] = useState(false);
+  const [weather, setWeather] = useState(false);
+
+  let getWeather = async (lat, long) => {
+    let res = await axios.get(
+      "http://api.openweathermap.org/data/2.5/weather",
+      {
+        params: {
+          lat: lat,
+          lon: long,
+          appid: "bfcfaae84810f423885b3cf28f9e31ff",
+          lang: "pt",
+          units: "metric",
+        },
+      }
+    );
+    setWeather(res.data);
+  };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      getWeather(position.coords.latitude, position.coords.longitude);
+      setLocation(true);
+    });
+  }, []);
+
+  if (location == false) {
+    return (
+      <View style={styles.container}>
+        <Text>Você precisa habilitar a localização no browser o/</Text>
+      </View>
+    );
+  } else if (weather == false) {
+    return (
+      <View style={styles.container}>
+        <Text>Carregando o clima...</Text>
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+
+        <Text>
+          Clima nas suas Coordenadas ({weather["weather"][0]["description"]})
+        </Text>
+        <Text>Temperatura atual: {weather["main"]["temp"]}°</Text>
+        <Text>Temperatura máxima: {weather["main"]["temp_max"]}°</Text>
+        <Text>Temperatura minima: {weather["main"]["temp_min"]}°</Text>
+        <Text>Pressão: {weather["main"]["pressure"]} hpa</Text>
+        <Text>Humidade: {weather["main"]["humidity"]}%</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    marginTop: 50,
+    marginLeft: 20,
   },
 });
